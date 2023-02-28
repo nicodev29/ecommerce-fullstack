66940e767759f9002d68639b18756ed991d0c4dc
Nicolas Garcia <nicodev1229@gmail.com>
2023-02-28 03:13:15 -0300

    frontparcial

--- 02-backend/src/main/java/com/example/ecommercefullstack/repository/ProductRepository.java
+++ 02-backend/src/main/java/com/example/ecommercefullstack/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.example.ecommercefullstack.repository;
 import com.example.ecommercefullstack.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
